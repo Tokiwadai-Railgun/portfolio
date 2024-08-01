07de5d684d47e249990099a4b258bf99a0a2a37e
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2024-08-01 19:10:56 +0200

    adaptation to cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,7 +5,15 @@ declare namespace App {
 	// interface Error {}
 	// interface Locals {}
 	// interface PageData {}
-	// interface Platform {}
+	interface Platform {
+    env: {
+      COUNTER: DurableObjectNamespace;
+    };
+    context: {
+      waitUntil(promise: Promise<any>): void;
+    };
+    caches: CacheStorage & { default: Cache };
+  }
 }
 
 declare module '*.md' {
